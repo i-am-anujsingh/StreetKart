7697199a03518526f4314452b1d75c10372e1624
Anuj <ianujsingh.1801@gmail.com>
2025-07-26 15:23:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+return (
+  <>
+  <h1 classNmae=''>StreetKart Project</h1>
+  <p className=''>Presented by</p>
+  <h2 className="">Dev Divers</h2>
+  </>
+  )
+}
+
+export default App
