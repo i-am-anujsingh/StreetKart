efa68dfa148b13448e39ccf0170cc2250e705ebe
manasvik209 <33247386-manasvik209@users.noreply.replit.com>
2025-07-27 05:18:18 +0000

    models files completed

---
+++ server/models/Order.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  vendorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Vendor',
+    required: true
+  },
+  itemId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Item',
+    required: true
+  },
+  itemName: {
+    type: String,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+  dateOfPurchase: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
